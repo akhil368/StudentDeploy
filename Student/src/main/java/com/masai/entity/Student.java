beadf572f8a9ea941558ecb6b0ad53481403ca93
unknown <akhil368>
2023-12-01 21:28:00 +0530

    Backend-Done

---
+++ Student/src/main/java/com/masai/entity/Student.java
@@ -0,0 +1,88 @@
+package com.masai.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String firstName;
+	private String lastName;
+	private double phone;
+	private LocalDate dob;
+	private String gender;
+	private String address;
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Student( String firstName, String lastName, double phone, LocalDate dob, String gender,
+			String address) {
+		super();
+		
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phone = phone;
+		this.dob = dob;
+		this.gender = gender;
+		this.address = address;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public double getPhone() {
+		return phone;
+	}
+	public void setPhone(double phone) {
+		this.phone = phone;
+	}
+	public LocalDate getDob() {
+		return dob;
+	}
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", phone=" + phone
+				+ ", dob=" + dob + ", gender=" + gender + ", address=" + address + "]";
+	}
+	
+	
+	
+}
